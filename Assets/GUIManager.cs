d23ed317f434dc785e01dd9ee410cb6484175f0e
AMeghnad <anshumanmeghnad@gmail.com>
2018-02-26 12:11:34 +1100

    Initial upload

---
+++ Assets/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour
+{
+    private float scrW, scrH;
+
+    // Use this for initialization
+    void Start()
+    {
+        scrH = Screen.height / 9;
+        scrW = Screen.width / 16;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Login()
+    {
+
+    }
+
+    void Register()
+    {
+
+    }
+}
